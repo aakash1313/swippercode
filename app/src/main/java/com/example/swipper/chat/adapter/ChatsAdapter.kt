0318d77957df038a3e71031df0b9db02a3c77953
akas <akas@microsoft.com>
2019-11-20 18:14:46 -0800

    Completed app

--- app/src/main/java/com/example/swipper/chat/adapter/ChatsAdapter.kt
+++ app/src/main/java/com/example/swipper/chat/adapter/ChatsAdapter.kt
@@ -8,6 +8,7 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.swipper.R
+import com.example.swipper.chat.ChatActivity
 import com.example.swipper.chat.models.Chat
 
 class ChatsAdapter(private var chats: ArrayList<Chat>): RecyclerView.Adapter<ChatsAdapter.ChatsViewHolder>() {
@@ -40,7 +41,8 @@ class ChatsAdapter(private var chats: ArrayList<Chat>): RecyclerView.Adapter<Cha
                     .into(image)
             }
             layout.setOnClickListener {
-
+                val intent = ChatActivity.newIntent(view.context, chat.chatId, chat.userId, chat.imageUrl, chat.otherUserId)
+                view.context.startActivity(intent)
             }
         }
 
---
+++ app/src/main/java/com/example/swipper/chat/models/Message.kt
@@ -0,0 +1,7 @@
+package com.example.swipper.chat.models
+
+data class Message(
+    val sentBy: String?= null,
+    val message: String?= null,
+    val time: String?= null
+)
